0b60218947826276fa9cc86d44298fa551548806
andrii.baranchuk <andreybaranchuk98@gmail.com>
2022-04-23 17:35:54 +0300

    Added GameBootstrapper.

---
+++ Assets/Scripts/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,32 @@
+﻿namespace Infrastructure
+{
+    using Services;
+    using Services.Tick;
+    using Services.Timer;
+    using UnityEngine;
+
+    public class GameBootstrapper : MonoBehaviour
+    {
+        [SerializeField] private TickService _tickService;
+        
+        private Game _game;
+        private ServiceLocator _serviceLocator;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+            
+            _serviceLocator = ServiceLocator.Container;
+            RegisterServices();
+            
+            _game = new Game();
+        }
+
+        private void RegisterServices()
+        {
+            _serviceLocator.RegisterSingle<ITickService>(_tickService);
+            _serviceLocator.RegisterSingle<ITimerDispatcher>
+                (new TimerDispatcher(_serviceLocator.Single<ITickService>()));
+        }
+    }
+}
