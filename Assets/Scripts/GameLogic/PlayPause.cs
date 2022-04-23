8ab7c56c0baa51b5ad26b1702f3229386d351cb2
andrii.baranchuk <andreybaranchuk98@gmail.com>
2022-04-23 17:36:10 +0300

    Added PlayPause.

---
+++ Assets/Scripts/GameLogic/PlayPause.cs
@@ -0,0 +1,20 @@
+﻿namespace GameLogic
+{
+    using Attributes;
+    using UnityEngine;
+
+    public class PlayPause : MonoBehaviour
+    {
+        [EditorButton]
+        public void Play()
+        {
+            Time.timeScale = 1;
+        }
+
+        [EditorButton]
+        public void Pause()
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
